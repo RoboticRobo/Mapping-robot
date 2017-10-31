f60e7609c7c243e02368de9b6a9dd503595038d7
RogerKSI <kittipong1996@hotmail.com>
2017-11-01 01:59:02 +0700

    convert depth to point in robot frame

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ assignment 3 of robotic course
 	2. copy .exe from /build/release and move to robot through teamviewer
 	3. run .exe
 	
-# Note 
+# Note kinect
 
 (MAT).rows = 480 
 
@@ -21,3 +21,61 @@ assignment 3 of robotic course
 unit of depth is millimeter
 
 nearest depth -> 500 
+
+# Note server localization
+
+WIFI
+	- ssid: ASUS-ISL2 
+	- pwd:	asusisl2
+
+URL server
+	- 192.168.1.59:8080
+
+Start camera
+	- 192.168.1.59:8080/camera/start
+	- response
+		+ Camera is already running
+		
+Localization
+	-  192.168.1.59:8080/pose
+
+	
+Snapshop
+	- 192.168.1.59:8080
+	
+Stop camera
+	- 192.168.1.59:8080/camera/stop
+ 
+Example-client 
+	- https://github.com/dhbaird/easywsclient/blob/master/example-client.cpp
+ 
+ 
+right bottom
+ id: 8	pos: 236.017, 163.929, 397.266	angle: 1.85717
+right top
+ id: 8	pos: 240.121, -164.212, 396.376	angle: -19.0336  
+left top
+ id: 8	pos: -211.61, -167.474, 406.818	angle: 4.2893
+left bottom
+ id: 8	pos: -190.826, 168.746, 408.63	angle: -1.11681
+ 
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
+id: 8   pos: 209.104, 164.246, 410.03   angle: -82.2055
--- _main.cpp
+++ _main.cpp
@@ -42,70 +42,154 @@ Mat depthImg;
 Mat colorImg;
 Mat indexImg;
 Mat pointImg;
+int score[1000][1000];
 
-void handle_message(const std::string & message)
+void update_score(int sx, int sy, int ex, int ey) {
+	int length = (int)floor(sqrt((ex - sx)*(ex - sx) + (ey - sy)*(ey - sy)));
+
+	double k = 1 / (double)length;
+
+	for (int i = 0; i < length; i++) {
+		int x, y;
+		x = sx + k*(i + 1)*(ex - sx);
+		y = sy + k*(i + 1)*(ey - sy);
+		if (i > (length - 2)) {
+			score[x][y] += 1;
+			score[x][y + 1] += 1;
+			score[x + 1][y] += 1;
+			score[x + 1][y + 1] += 1;
+		}
+		else {
+			score[x][y] += -1;
+			score[x][y + 1] += -1;
+			score[x + 1][y] += -1;
+			score[x + 1][y + 1] += -1;
+		}
+	}
+
+	score[ex][ey] += 2;
+}
+
+void convert_to_world_frame(double posx, double posy, double angle, double end_robot_x, double end_robot_y, double& end_world_x, double& end_world_y) {
+	// do something
+
+
+	// end_world_x = ...;
+	// end_world_y = ...;
+}
+
+void walk(double vl, double vr) {
+
+	int velL = (int)(vl*Create_MaxVel);
+	int velR = (int)(vr*Create_MaxVel);
+
+	robot.DriveDirect(velL, velR);
+	Sleep(100);
+	robot.DriveDirect(0, 0);
+}
+
+void handle_response(const std::string & message)
 {
-
-//	kin.GrabData(depthImg, colorImg, indexImg, pointImg);
+	//	kin.GrabData(depthImg, colorImg, indexImg, pointImg);
 	//imshow("depthImg", depthImg);
 	//imshow("colorImg", colorImg);
 
 	string res = message.c_str();
-	cout << res << endl;
-	//res = res.substr(3, res.size() - 3);
+	res = res.substr(3, res.size() - 3);
 
-	//while (res.find("<br/>") != string::npos) {
-	//	if (res.find("id: 8") == 0) {
-	//		res = res.substr(0, res.find("<br/>"));
-	//		
-	//		double posx, posy, posz;
-	//		double angle;
-	//		posx = atof(res.substr(res.find("pos:") + 5, res.find(",") - res.find("pos:") - 5).c_str());
-	//		res = res.substr(res.find(",") + 2);
-	//		posy = atof(res.substr(0, res.find(",")).c_str());
-	//		res = res.substr(res.find(",") + 2);
-	//		posz = atof(res.substr(0, res.find(",")).c_str());
-	//		res = res.substr(res.find("angle: ") + 8);
-	//		double neg = 1;
-	//		if (res[0] == '-') {
-	//			neg = -1;
-	//			res = res.substr(1);
-	//		}
-	//		angle = neg * atof(res.c_str());
+	while (res.find("<br/>") != string::npos) {
+		///////////////////////////////////////////////
+		// find my robot in response
+		///////////////////////////////////////////////
+		if (res.find("id: 8") == 0) {
+			res = res.substr(0, res.find("<br/>"));
+			
+			///////////////////////////////////////////////
+			// extract position and angle from response
+			///////////////////////////////////////////////
+			double posx, posy, posz;
+			double angle;
+			posx = atof(res.substr(res.find("pos:") + 5, res.find(",") - res.find("pos:") - 5).c_str());
+			res = res.substr(res.find(",") + 2);
+			posy = atof(res.substr(0, res.find(",")).c_str());
+			res = res.substr(res.find(",") + 2);
+			posz = atof(res.substr(0, res.find(",")).c_str());
+			res = res.substr(res.find("angle: ") + 7);
 
-	//		cout << posx << " " << posy <<" " <<posz <<" " <<angle  << " end\n";
+			double neg = 1;
+			if (res[0] == '-') {
+				neg = -1;
+				res = res.substr(1);
+			}
+			angle = neg * atof(res.c_str());
 
-	//		break;
-	//	}
-	//	else 
-	//		res = res.substr(res.find("<br/>") + 5, res.size() - res.find("<br/>") - 5);
-	//}
-	//cout << "Sasad\n";
+			//cout << posx << " " << posy <<" " <<posz <<" " <<angle  << " end\n";
+
+			for (int i = 0; i < 640; i++) {
+				///////////////////////////////////////////////
+				// find point in robot frame from depth
+				///////////////////////////////////////////////
+				double end_y_robot = depthImg.at<USHORT>(i, 240);
+				double end_x_robot = (i - 320.0) / 575.0 * end_y_robot;
+
+				///////////////////////////////////////////////
+				// convert point in robot frame to world frame
+				///////////////////////////////////////////////
+				double end_x_world, end_y_world;
+				convert_to_world_frame(posx, posy, angle, end_x_robot, end_y_robot, end_x_world , end_y_world);
+
+				///////////////////////////////////////////////
+				// update score from point
+				///////////////////////////////////////////////
+				update_score(posx, posy, end_x_world, end_y_world);
+			}
+
+			///////////////////////////////////////////////
+			// Boss: find path to grid that score 0 (BFS ??)
+			///////////////////////////////////////////////
+			// ---
+
+			///////////////////////////////////////////////
+			// walk to first point of Boss's path
+			///////////////////////////////////////////////
+			// ---
+
+		}
+		else 
+			res = res.substr(res.find("<br/>") + 5, res.size() - res.find("<br/>") - 5);
+	}
+	cout << "Sasad\n";
 }
 
 int main()
 {
+	cvNamedWindow("Robot");
 
+	///////////////////////////////////////////////
 	// initialize robot
-/*
+	///////////////////////////////////////////////
+	/*
 	if (!robot.Connect(Create_Comport))
 	{
 		cout << "Error : Can't connect to robot @" << Create_Comport << endl;
 		return -1;
 	}
-
 	//robot.DriveDirect(0, 0);
-	cvNamedWindow("Robot");
-*/
+	*/
 
+	///////////////////////////////////////////////
 	// initialize kinect
-	kin = KinectConnector();
-	if (!kin.Connect()) {
-		cout << "Error : Can't connect to kinect" << endl;
-		return 1;
-	}
+	///////////////////////////////////////////////
+	//kin = KinectConnector();
+	//if (!kin.Connect()) {
+	//	cout << "Error : Can't connect to kinect" << endl;
+	//	return 1;
+	//}
 
+
+	///////////////////////////////////////////////
 	// initialize socket
+	///////////////////////////////////////////////
 	INT rc;
 	WSADATA wsaData;
 	rc = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -116,45 +200,21 @@ int main()
 	using easywsclient::WebSocket;
 	WebSocket::pointer wp = WebSocket::from_url("ws://192.168.1.59:8080/pose");
 
-
+	///////////////////////////////////////////////
+	// slam
+	///////////////////////////////////////////////
 	while (true)
 	{
 		wp->poll();
 		wp->send("");
-		wp->dispatch(handle_message);
+		wp->dispatch(handle_response);
 		
-
-		//char c = cvWaitKey(30);
-		//if (c == 27) break;
-
-		//double vx, vz;
-		//vx = vz = 0.0;
-
-		//switch (c)
-		//{
-		//case 'w': vx = +1; break;
-		//case 's': vx = -1; break;
-		//case 'a': vz = +1;  break;
-		//case 'd': vz = -1;  break;
-		//case ' ': vx = vz = 0; break;
-		//case 'c': robot.Connect(Create_Comport); break;
-		//}
-
-		//double vl = vx - vz;
-		//double vr = vx + vz;
-
-
-		//int velL = (int)(vl*Create_MaxVel);
-		//int velR = (int)(vr*Create_MaxVel);
-
-		//robot.DriveDirect(velL, velR);
-		//robot.ReadData(robotData);
-
 		cvWaitKey(100);
 	}
 
+	///////////////////////////////////////////////
 	// close everything;
-	
+	///////////////////////////////////////////////
 	//robot.Disconnect();
 	wp->close();
 	delete wp;
